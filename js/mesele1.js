bad43ff612ae6562b2916270b8cce5b86dd1e971
IbraIbrahimov <iibragimli4@gmail.com>
2019-08-01 19:35:29 +0400

    ev-tapshirigi_ibrahim

--- js/mesele1.js
+++ js/mesele1.js
@@ -2,7 +2,7 @@ let a=prompt('üç bucağın 1ci terefinin uzunluğunu qeyd edin');
 let b=prompt('üç bucağın 2ci terefinin uzunluğunu qeyd edin');
 let c=prompt('üç bucağın 3cü terefin uzunluğunu qeyd edin');
 
-if(a==b==c){
+if(a==b && a==c){
  alert('uc bucagi qurmag munkundur');
 
 
